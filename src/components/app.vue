74318031883ff71ad5830a525ff341c81f835aa7
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-01-30 16:02:26 +0500

    Framework7+webpack+scss install

---
+++ src/components/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <f7-app :params="f7params">
+    <!-- Left panel with cover effect when hidden -->
+    <f7-panel left cover theme-dark :visible-breakpoint="960">
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Left Panel"></f7-navbar>
+          <f7-block-title>Left View Navigation</f7-block-title>
+          <f7-list>
+            <f7-list-item link="/left-page-1/" title="Left Page 1"></f7-list-item>
+          </f7-list>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
+
+    <!-- Your main view, should have "view-main" class -->
+    <f7-view main class="safe-areas" url="/"></f7-view>
+  </f7-app>
+</template>
+<script>
+import routes from "../js/routes.js";
+
+export default {
+  data() {
+    return {
+      // Framework7 Parameters
+      f7params: {
+        name: "ND-test", // App name
+        theme: "auto", // Automatic theme detection
+        // App routes
+        routes: routes
+      },
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    this.$f7ready(f7 => {
+      // Call F7 APIs here
+    });
+  }
+};
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar :sliding="false">
+      <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title sliding>ND-test</f7-nav-title>
+    </f7-navbar>
+    <!-- Page content-->
+
+  </f7-page>
+</template>
