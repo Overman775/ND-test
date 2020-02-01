3e1dba8bd0ee6ce3ed004ac35811edeebcdece43
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-02-01 11:33:23 +0500

    Add vuex-persistedstate to save state

--- src/js/store.js
+++ src/js/store.js
@@ -1,13 +1,27 @@
+// Import Vuex persistedstate plugin
+import createPersistedState from "vuex-persistedstate";
+
 const store = {
+  plugins: [
+    createPersistedState({
+      paths: "settings",
+      reducer: state => ({
+        settings: state.settings
+      })
+    })
+  ],
   state: {
-    images: []
+    images: [],
+    settings: {
+      darkMode: false
+    }
   },
   getters: {
     IMAGES_LIST: state => {
       return state.images;
     },
-    IMAGE: (state) => (id) => {
-        return state.images[id]
+    IMAGE: state => id => {
+      return state.images[id];
     }
   },
   mutations: {
@@ -15,11 +29,14 @@ const store = {
       state.images = payload;
     },
     RESET_IMAGES_LIST: state => {
-        state.images.splice(0, state.images.length);
+      state.images.splice(0, state.images.length);
     },
     ADD_IMAGE: (state, payload) => {
-        state.images.push(payload);
-      } 
+      state.images.push(payload);
+    },
+    SET_SETTINGS: (state, payload) => {
+      state.settings[payload.prop] = payload["val"];
+    }
   },
   actions: {}
 };
